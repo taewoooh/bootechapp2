0000f4f39494860e46a3ccbee62cc2bc9cae655d
taewoooh <taewoooh88@gmail.com>
2025-12-04 00:11:25 +0900

    ㅎㅎ

---
+++ BaseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BaseViewController.swift
+//  bootechapp
+//
+//  Created by 오태우 on 12/3/25.
+//
+
+import Foundation
---
+++ Dimensions.swift
@@ -0,0 +1,8 @@
+//
+//  Dimensions.swift
+//  bootechapp
+//
+//  Created by 오태우 on 11/27/25.
+//
+
+import Foundation
